6776de06bcc59c067a0317cc3659b17f3c74c2b7
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-10-22 17:26:51 +0300

    Add files via upload

---
+++ DB Apps Introduction Exercise/Constans.java
@@ -0,0 +1,10 @@
+package JavaDBAppsIntroduction;
+
+enum Constans {
+    ;
+    static  final String USER_KEY = "user";
+    static  final String USER_VALUE = "root";
+    static  final String PASSWORD_KEY = "password";
+    static  final String PASSWORD_VALUE = "Elhovo123elhovo";
+    static  final String JDBC_URL = "jdbc:mysql://localhost:3306/minions_db";
+}
