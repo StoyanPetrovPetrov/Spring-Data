09068efbd7372985b1e2dce379a44084ec2537ab
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-11-05 19:23:36 +0200

    Add files via upload

---
+++ Hibernate Code First Exercise/BillsPaymentSystem/src/main/java/entities/BaseEntity.java
@@ -0,0 +1,16 @@
+package entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+}
