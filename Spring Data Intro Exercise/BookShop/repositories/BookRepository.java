9698ec0fcc8e2bb8dbd979bed715cc0cda479ad7
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-11-09 19:15:23 +0200

    Add files via upload

---
+++ Spring Data Intro Exercise/BookShop/repositories/BookRepository.java
@@ -0,0 +1,17 @@
+package com.softuni.bookshop.repositories;
+
+import com.softuni.bookshop.domein.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
+    Optional<List<Book>> findAllByReleaseDateAfter(LocalDate localDate);
+
+    Optional<List<Book>> findAllByAuthorFirstNameAndAuthorLastNameOrderByReleaseDateDescTitleAsc(String firstName, String lastName);
+}
+
