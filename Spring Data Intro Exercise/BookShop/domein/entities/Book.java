16bf52a8f1a94534ee22063ec7c4553ce44b806e
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-11-09 19:14:48 +0200

    Add files via upload

---
+++ Spring Data Intro Exercise/BookShop/domein/entities/Book.java
@@ -0,0 +1,46 @@
+package com.softuni.bookshop.domein.entities;
+
+import com.softuni.bookshop.domein.enums.AgeRestriction;
+import com.softuni.bookshop.domein.enums.EditionType;
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Book extends BaseEntity{
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private EditionType editionType;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer copies;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Enumerated(EnumType.STRING)
+    private AgeRestriction ageRestriction;
+
+    @ManyToOne
+    private Author author;
+
+    @ManyToMany
+    private Set<Category> categories;
+}
---
+++ Spring Data Intro Exercise/BookShop/domein/entities/Category.java
@@ -0,0 +1,19 @@
+package com.softuni.bookshop.domein.entities;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Category extends BaseEntity{
+    @Column
+    private String name;
+}
