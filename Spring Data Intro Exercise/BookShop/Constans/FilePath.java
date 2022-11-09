84b09804c00408eca86cf21b18d03e24d9706217
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-11-09 19:14:24 +0200

    Add files via upload

---
+++ Spring Data Intro Exercise/BookShop/Constans/FilePath.java
@@ -0,0 +1,12 @@
+package com.softuni.bookshop.Constans;
+
+public enum FilePath {
+    ;
+    public static final String RESOURCE_URL = "src/main/resources/dbContent/";
+
+    public static final String BOOK_FILE_NAME = "books.txt";
+
+    public static final String CATEGORY_FILE_NAME = "categories.txt";
+
+    public static final String AUTHOR_FILE_NAME = "authors.txt";
+}
