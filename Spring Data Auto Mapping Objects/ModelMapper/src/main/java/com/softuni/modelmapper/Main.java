20961deb0678016e31967aa63df3909617266fa1
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-11-11 20:26:33 +0200

    Update Main.java

--- Spring Data Auto Mapping Objects/ModelMapper/src/main/java/com/softuni/modelmapper/Main.java
+++ Spring Data Auto Mapping Objects/ModelMapper/src/main/java/com/softuni/modelmapper/Main.java
@@ -10,16 +10,32 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 
 @Component
-public class Main implements CommandLineRunner {
+public class ModelMapperMain implements CommandLineRunner {
 ModelMapper mapper = new ModelMapper();
     @Override
     public void run(String... args) throws Exception {
-       Address address = new Address("Bulgaria","Sofia");
-        Employee employee = new Employee("First", BigDecimal.TEN,address);
+        ModelMapper mapper = new ModelMapper();
+  //       PropertyMap<Employee, EmployeeDTO> propertyMap = new PropertyMap<>() {
+//            @Override
+//            protected void configure() {
+//                map().setCity(source.getAddress().getCity());
+//            }
+//        };
+//        mapper.addMappings(propertyMap);
+//        mapper.validate();
 
-       EmployeeDTO employeeDTO = mapper.map(employee, EmployeeDTO.class);
+        TypeMap<Employee, EmployeeDTO> typeMap = mapper.createTypeMap(Employee.class, EmployeeDTO.class);
+        typeMap.addMappings(mapping -> mapping.map(
+        source -> source.getAddress().getCity(),
+        EmployeeDTO::setAddressCity)
+                );
+//        typeMap.validate();
 
-       // EmployeeDto employeeDto ?
+        Address address = new Address("Bulgaria", "Sofia");
+        Employee employee = new Employee("First", BigDecimal.TEN, address);
+
+        EmployeeDTO employeeDTO = typeMap.map(employee);
+
         System.out.println(employeeDTO);
     }
 }
