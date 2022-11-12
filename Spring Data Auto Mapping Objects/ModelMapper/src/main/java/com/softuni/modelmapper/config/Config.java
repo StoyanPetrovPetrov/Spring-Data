413cc3f6b4e8ba4b9aca9c8b5504bc21abac8fb1
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-11-12 15:29:10 +0200

    Add files via upload

---
+++ Spring Data Auto Mapping Objects/ModelMapper/src/main/java/com/softuni/modelmapper/config/Config.java
@@ -0,0 +1,28 @@
+package com.softuni.modelmapper.config;
+
+import com.softuni.modelmapper.repositories.AddressRepository;
+import com.softuni.modelmapper.services.AddressService;
+import com.softuni.modelmapper.services.AddressServiceImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Scanner;
+
+@Configuration
+public class Config {
+    @Bean
+    public ModelMapper createModelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public Scanner createScanner() {
+        return new Scanner(System.in);
+    }
+
+    @Bean
+    public AddressService createAddressService(AddressRepository repo, ModelMapper mapper) {
+        return new AddressServiceImpl(repo, mapper);
+    }
+}
