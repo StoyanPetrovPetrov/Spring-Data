c6f0ce5db88922118effef4d74fbd049898c1c80
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-11-19 20:25:38 +0200

    Add files via upload

---
+++ JSON Processing  Exercise/ProductShop.java/models/productsShop/dtos/ProductsInRangeDto.java
@@ -0,0 +1,36 @@
+package com.softuni.productsshop.models.productsShop.dtos;
+
+import com.google.gson.annotations.Expose;
+
+public class ProductsInRangeDto {
+    @Expose
+    private String name;
+    @Expose
+    private String price;
+    @Expose
+    private String seller;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+}
