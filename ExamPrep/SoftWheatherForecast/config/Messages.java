a922989332d65f7367cab62a8ffcaff4476412fd
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-11-28 20:45:27 +0200

    Add files via upload

---
+++ ExamPrep/SoftWheatherForecast/config/Messages.java
@@ -0,0 +1,13 @@
+package softuni.exam.config;
+
+public enum Messages {
+    ;
+    public static final String INVALID_COUNTRY = "Invalid country%n";
+    public static final String VALID_COUNTRY_FORMAT= "Successfully imported country %s - %s%n";
+
+    public static final String INVALID_CITY = "Invalid city%n";
+    public static final String VALID_CITY_FORMAT= "Successfully imported city %s - %d%n";
+
+    public static final String INVALID_FORECAST = "Invalid forecast";
+    public static final String VALID_FORECAST_FORMAT = "Successfully imported forecast %s - %d%n";
+}
