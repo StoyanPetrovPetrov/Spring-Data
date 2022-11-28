8a403de8397452f4e44051e95ae27c4ad11d91e9
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-11-28 20:45:49 +0200

    Add files via upload

---
+++ ExamPrep/SoftWheatherForecast/constants/Paths.java
@@ -0,0 +1,10 @@
+package softuni.exam.constants;
+
+import java.nio.file.Path;
+
+public enum Paths {
+    ;
+    public static final String COUNTRIES_PATH = "src/main/resources/files/json/countries.json";
+
+    public static final Path FORECASTS_PATH = Path.of("src/main/resources/files/xml/forecasts.xml");
+}
