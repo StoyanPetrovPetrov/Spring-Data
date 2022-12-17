585612ef1d25291df6cdb21847caeaffaf3c979d
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-12-17 17:21:15 +0200

    Create InstagraphLiteApplication.java

---
+++ ExamPrep/Instagraph Lite/java/InstagraphLiteApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagraphLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagraphLiteApplication.class, args);
+    }
+
+}
