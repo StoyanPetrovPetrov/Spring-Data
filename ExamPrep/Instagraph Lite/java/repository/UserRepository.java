e3f079eac3f488039a3a8399b1180eb8b9fba80b
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-12-17 17:22:36 +0200

    Add files via upload

---
+++ ExamPrep/Instagraph Lite/java/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package softuni.exam.instagraphlite.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import softuni.exam.instagraphlite.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User AS u ORDER BY u.posts.size DESC, u.id ASC")
+    List<User> findAllUsersOrderedByCountOfPostsDescUserIdAsc();
+}
