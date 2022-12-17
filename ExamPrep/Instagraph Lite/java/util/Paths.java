85fdda9c70920d9e89597c4dffb0a9036f7e0c46
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-12-17 17:23:26 +0200

    Add files via upload

---
+++ ExamPrep/Instagraph Lite/java/util/Paths.java
@@ -0,0 +1,16 @@
+package softuni.exam.instagraphlite.util;
+
+import java.nio.file.Path;
+
+public enum Paths {
+    ;
+
+    public static final Path PICTURES_JSON_PATH =
+            Path.of("src", "main", "resources", "files", "pictures.json");
+
+    public static final Path POSTS_XML_PATH =
+            Path.of("src", "main", "resources", "files", "posts.xml");
+
+    public static final Path USERS_JSON_PATH =
+            Path.of("src", "main", "resources", "files", "users.json");
+}
